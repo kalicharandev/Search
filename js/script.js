3c7cc8cda996264d1963550c5cb438eaf9c55a14
unknown <meiltokalicharan@gmail.com>
2021-05-13 18:12:25 +0100

    modify color and script

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var a = 5;
+var x = (100 + 20) * a;
+document.getElementById("demo").innerHTML = "roshini";
+function onSubmit() {
+  document.getElementById("demo").innerHTML =document.getElementById("search").value;
+}
